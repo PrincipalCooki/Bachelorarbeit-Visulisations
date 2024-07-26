323bac656ec8cb3dc6c52485a5f97fe633995324
PrincipalCookie <cookieprincipal@gmail.com>
2024-07-26 19:56:01 +0200

    plot finished

--- good_bad_basis.py
+++ good_bad_basis.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
 
-# Define a function to generate lattice points
+
 def generate_lattice_points(basis, range_vals):
     points = []
     for i in range_vals:
@@ -10,31 +11,42 @@ def generate_lattice_points(basis, range_vals):
             points.append(point)
     return np.array(points)
 
-# Define the original basis and a different basis
-basis1 = np.array([[3.6, 2.6], [5, 3.8]])  # standard basis
-basis2 = np.array([[0.8, 0.2], [0.2, -0.8]])  # Different basis
 
-# Generate lattice points
-range_vals = np.arange(-5, 6)
+def add_parallelepiped(ax, basis):
+    points = np.array([[0, 0], basis[0], basis[0] + basis[1], basis[1]])
+    polygon = Polygon(points, closed=True, color='blue', alpha=0.3)
+    ax.add_patch(polygon)
+
+
+b_1 = np.array([3.2, 2.4])
+b_2 = np.array([3.7, 4.2])
+basis1 = np.array([b_1, b_2])  # standard basis
+basis2 = np.array([-b_2 + b_1, 3*b_1 - 2 * b_2])  # Different basis
+
+range_vals = np.arange(-100, 100)
 lattice_points1 = generate_lattice_points(basis1, range_vals)
 lattice_points2 = generate_lattice_points(basis2, range_vals)
 
-# Plot lattice points
-plt.figure(figsize=(8, 8))
-plt.scatter(lattice_points1[:, 0], lattice_points1[:, 1], color='black', s=10)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 7))
 
-# Plot basis vectors
-origin = np.array([0, 0])
-plt.quiver(*origin, *basis1[0], color='r', scale=1, scale_units='xy', angles='xy', width=0.001)
-plt.quiver(*origin, *basis1[1], color='r', scale=1, scale_units='xy', angles='xy', width=0.001)
-plt.quiver(*origin, *basis2[0], color='g', scale=1, scale_units='xy', angles='xy', width=0.001)
-plt.quiver(*origin, *basis2[1], color='g', scale=1, scale_units='xy', angles='xy', width=0.001)
+ax1.scatter(lattice_points1[:, 0], lattice_points1[:, 1], color='black', s=10)
+ax1.quiver(0, 0, *basis1[0], color='r', scale=1, scale_units='xy', angles='xy', width=0.004)
+ax1.quiver(0, 0, *basis1[1], color='r', scale=1, scale_units='xy', angles='xy', width=0.004)
+add_parallelepiped(ax1, basis1)
+ax1.set_xlim(-10, 10)
+ax1.set_ylim(-10, 10)
+ax1.axhline(0, color='black', lw=1)
+ax1.axvline(0, color='black', lw=1)
+ax1.grid(True)
 
-# Customize plot
-plt.xlim(-10, 10)
-plt.ylim(-10, 10)
-plt.axhline(0, color='black', lw=1)
-plt.axvline(0, color='black', lw=1)
-plt.grid(True)
-plt.title('Lattice Points with Different Bases')
+ax2.scatter(lattice_points2[:, 0], lattice_points2[:, 1], color='black', s=10)
+ax2.quiver(0, 0, *basis2[0], color='g', scale=1, scale_units='xy', angles='xy', width=0.004)
+ax2.quiver(0, 0, *basis2[1], color='g', scale=1, scale_units='xy', angles='xy', width=0.004)
+add_parallelepiped(ax2, basis2)
+ax2.set_xlim(-10, 10)
+ax2.set_ylim(-10, 10)
+ax2.axhline(0, color='black', lw=1)
+ax2.axvline(0, color='black', lw=1)
+ax2.grid(True)
+
 plt.show()
